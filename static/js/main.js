14fd2fa5d11eea398e5b1cdf90319757d60c63fa
Ruben <rubenca2312@gmail.com>
2023-07-06 18:18:40 +0200

    add function max-img and fix center img

--- static/js/main.js
+++ static/js/main.js
@@ -1,3 +1,15 @@
 window.addEventListener('scroll', () => {
     document.body.style.setProperty('--scroll', window.pageYOffset / (document.body.offsetHeight - window.innerHeight));
 }, false);
+
+function getFullscreen(element){
+    if(element.requestFullscreen) {
+        element.requestFullscreen();
+    } else if(element.mozRequestFullScreen) {
+        element.mozRequestFullScreen();
+    } else if(element.webkitRequestFullscreen) {
+        element.webkitRequestFullscreen();
+    } else if(element.msRequestFullscreen) {
+        element.msRequestFullscreen();
+    }
+}
